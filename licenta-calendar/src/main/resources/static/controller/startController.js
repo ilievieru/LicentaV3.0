31b051cfec9f14df2f1167ce7dc83a0d7d5cbf68
Ness <User@ness.com>
2017-03-20 17:15:11 +0200

    Start work on UI. Calendar Added

--- licenta-calendar/src/main/resources/static/controller/startController.js
+++ licenta-calendar/src/main/resources/static/controller/startController.js
@@ -1,9 +1,22 @@
 app.controller("startController", function ($scope, $http) {
     console.log("start controller");
+    $scope.results="Text for test";
 
     $scope.ready = function () {
         return true;
     }
 
-    $scope.statProcess = function () {}
+    $scope.statProcess = function () {
+        $http({
+            method: 'GET',
+            url: '/BordaVotingEndpoint',
+            params: {input: testFirstTab}
+        })
+            .success(function (results) {
+                console.log(results);
+            })
+            .error(function () {
+                console.log("error");
+            });
+    }
 });
