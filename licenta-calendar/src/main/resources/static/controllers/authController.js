aeeb11f7bc85b573b8b20aed99cd7ec62fa6df93
ilievieru <ilie_vieru_2010@yahoo.com>
2017-05-28 18:57:11 +0300

    Ui Calendar work

--- licenta-calendar/src/main/resources/static/controllers/authController.js
+++ licenta-calendar/src/main/resources/static/controllers/authController.js
@@ -1,6 +1,8 @@
 var app = angular.module("LogIn", ['ngMaterial']);
 app.controller("authController", function ($scope, $http) {
     console.log("authController...");
+    $scope.login = false;
+    $scope.beforeLogin = true;
     $scope.auth = function () {
         $scope.userName = document.getElementById("userName").value;
         $scope.password = document.getElementById("password").value;
@@ -14,10 +16,18 @@ app.controller("authController", function ($scope, $http) {
         })
             .success(function (results) {
                 console.log(results);
-                window.location.replace("mainPage.html");
+                if (results.Success == true) {
+                    window.location.replace("mainPage.html");
+                }
+                else {
+                    $scope.login = true;
+                    $scope.beforeLogin = false;
+                }
             })
             .error(function () {
                 console.log("error");
+                $scope.login = true;
+                $scope.beforeLogin = false;
             });
     }
 });
